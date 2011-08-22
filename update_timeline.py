077dd94f798e47670d5680d83ff6643cc41e28a7
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2011-08-22 17:45:59 -0600

    Moved file names to config.py

--- update_timeline.py
+++ update_timeline.py
@@ -1,9 +1,9 @@
 import logging
 import sqlite3
 
-from authdata import *
+from config import *
 
-logging.basicConfig(filename='./update_timeline.log',
+logging.basicConfig(filename=log_file,
                     level=logging.INFO,
                     format='%(asctime)s %(message)s')
 
@@ -83,7 +83,7 @@ def insert_timeline(tl, file_name):
     
 if __name__ == '__main__':
 
-    db_name = './tweets.sqlite'
+    #db_name = './tweets.sqlite'
     #create_db(db_name)
     api = auth()
     timeline = get_timeline(api)
